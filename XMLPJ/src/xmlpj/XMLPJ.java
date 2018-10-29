8c79f7991cf8130dcacd840b0a4f998bd05ee650
NgoMinh <33169628+AdlerkoV@users.noreply.github.com>
2018-10-30 06:45:14 +0700

    addpj

--- XMLPJ/src/xmlpj/XMLPJ.java
+++ XMLPJ/src/xmlpj/XMLPJ.java
@@ -3,19 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Crawler;
+package xmlpj;
 
 import Constant.AppConstant;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletContext;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.Characters;
@@ -26,13 +32,44 @@ import javax.xml.stream.events.XMLEvent;
  *
  * @author adler
  */
-public class MybossCategoriesCrawler extends BaseCrawler {
+public class XMLPJ {
 
-    public MybossCategoriesCrawler(ServletContext context) {
-        super(context);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        getCategories("http://www.myboss.vn/");
     }
 
-    public Map<String, String> getCategories(String url) {
+//    private ServletContext context;
+//
+//    public BaseCrawler(ServletContext context) {
+//        this.context = context;
+//    }
+//
+//    public ServletContext getContext() {
+//        return context;
+//    }
+    protected static BufferedReader getBufferedReaderForURL(String urlString)
+            throws MalformedURLException, UnsupportedEncodingException, IOException {
+        URL url = new URL(urlString);
+        URLConnection connection = url.openConnection();
+        connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64");
+        InputStream is = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+        return reader;
+    }
+
+    protected static XMLEventReader parseStringToXMLEventReader(String xmlSection)
+            throws UnsupportedEncodingException, XMLStreamException {
+        byte[] byteArray = xmlSection.getBytes("UTF-8");
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(byteArray);
+        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+        XMLEventReader reader = inputFactory.createXMLEventReader(inputStream);
+        return reader;
+    }
+
+    public static Map<String, String> getCategories(String url) {
         BufferedReader reader = null;
         try {
             reader = getBufferedReaderForURL(url);
@@ -52,28 +89,29 @@ public class MybossCategoriesCrawler extends BaseCrawler {
                 }
                 if (line.contains("<a href=\"thiet-bi-choi-game-c1\"")) {
                     isFound = true;
+                    System.out.println(line);
                 }
             }
             return stAXparserForCategories(document);
         } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(BaseCrawler.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         } catch (IOException ex) {
-            Logger.getLogger(BaseCrawler.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         } catch (XMLStreamException ex) {
-            Logger.getLogger(MybossCategoriesCrawler.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
         } finally {
             try {
                 if (reader != null) {
                     reader.close();
                 }
             } catch (IOException ex) {
-                Logger.getLogger(BaseCrawler.class.getName()).log(Level.SEVERE, null, ex);
+                ex.printStackTrace();
             }
         }
         return null;
     }
 
-    public Map<String, String> stAXparserForCategories(String document)
+    public static Map<String, String> stAXparserForCategories(String document)
             throws UnsupportedEncodingException, XMLStreamException {
         document = document.trim();
         XMLEventReader eventReader = parseStringToXMLEventReader(document);
@@ -83,17 +121,19 @@ public class MybossCategoriesCrawler extends BaseCrawler {
             if (event.isStartElement()) {
                 StartElement startElement = event.asStartElement();
                 String tagName = startElement.getName().getLocalPart();
+                System.out.println(event);
+//                System.out.println(tagName);
                 if ("a".equals(tagName)) {
                     Attribute attrHref = startElement.getAttributeByName(new QName("href"));
                     String link = AppConstant.urlMyboss + attrHref.getValue();
                     event = (XMLEvent) eventReader.next();
                     Characters character = event.asCharacters();
                     categories.put(link, character.getData());
+                    System.out.println(character);
                 }
             }
         }
         return categories;
     }
-
 
 }
